741abe286f6a2391476c3ee104b3413cc1dd0ec0
DJMojo <mortierjo@gmail.com>
2016-05-14 20:06:14 +0200

    Change message with e=mc2

--- src/main/java/fr/djmojo/workout/Server.java
+++ src/main/java/fr/djmojo/workout/Server.java
@@ -1,6 +1,10 @@
 package fr.djmojo.workout;
 
 import static spark.Spark.get;
+import static javax.measure.unit.SI.KILOGRAM;
+import javax.measure.quantity.Mass;
+import org.jscience.physics.model.RelativisticModel;
+import org.jscience.physics.amount.Amount;
 
 /**
  * Created by DJMojo on 14/05/16.
@@ -8,7 +12,11 @@ import static spark.Spark.get;
 public class Server {
 
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        get("/hello", (req, res) -> {
+            RelativisticModel.select();
+            Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
+            return "E=mc^2: 12 GeV = " + m.toString();
+        });
     }
 
 }
